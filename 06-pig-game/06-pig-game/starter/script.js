fc1256d7cc1c6b6f1b36e8a336641068c89369fb
kookiejeon <mynameisjungkook197@gmail.com>
2025-09-27 13:04:04 +0800

    feat: add pig game starter files and documentation

---
+++ 06-pig-game/06-pig-game/starter/script.js
@@ -0,0 +1,38 @@
+'use strict';
+// Element selections
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
+init();
+
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    
+  }
+});
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+   
+  }
+});
